5711ac899911ff63d125da4b837a5a83cf1885d8
sohaibsalman13 <86664458+sohaibsalman13@users.noreply.github.com>
2022-06-21 18:11:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,53 +1,87 @@
 import requests
-import tkinter as tk
+from tkinter import messagebox
+from tkinter import *
+from configparser import ConfigParser
+
+url = "https://api.openweathermap.org/data/2.5/weather?q={}&appid={}"
+
+# reading the api key from the config file
+config_file = 'config.ini'
+config = ConfigParser()
+config.read(config_file)
+api_key = config['api_key']['key']
 
 
-def func(canvas):
-    city = textField.get()
-    api = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=52d200dd0dee27f8c8351154b9c6032c"
-    json_data = requests.get(api).json()
+def get_weather(city):
+    result = requests.get(url.format(city, api_key))
 
-    weather = json_data['weather'][0]['main']
-    humid = json_data['main']['humidity']
-    temp = int(json_data['main']['temp']-273.15)
-    min_temp = int(json_data['main']['temp_min']-273.15)
-    max_temp = int(json_data['main']['temp_max']-273.15)
-    pressure = json_data['main']['pressure']
-    wind = json_data['wind']['speed']
+    if result:
 
-    final_info = weather + "\n" + str(temp) + "°C"
-    final_data = "\n" + "Max temp: " + str(max_temp) + "\n" \
-                 + "Min temp: " + str(min_temp) + "\n" + "Pressure: " \
-                 + str(pressure) + "\n" + "Humidity: " + str(humid) + "\n" \
-                 + "Wind Speed: " + str(wind)
+        # getting the data from json
+        city = result.json()['name']
+        country = result.json()['sys']['country']
+        weather = result.json()['weather'][0]['main']
+        icon = result.json()['weather'][0]['icon']
+        humid = result.json()['main']['humidity']
+        temp = int(result.json()['main']['temp'] - 273.15)
 
-    L1.config(text = final_info)
-    L2.config(text = final_data)
+        # tuple to save all the value
+        final = (city, country, temp, icon, weather, humid)
+        return final
+    else:
+        return None
 
 
-canvas = tk.Tk()
-canvas.geometry("600x500")
-canvas.title("Weather App")
+def search():
+    city = city_text.get()
 
-f = ("New Times Roman", 15, "bold",)
-t = ("New Times Roman", 30, "bold")
+    # calling the get_weather function
+    weather = get_weather(city)
+    if weather:
+        # setting the label values from the get_weather function
+        location_lbl['text'] = '{}, {}'.format(weather[0], weather[1])
+        icon_ref = PhotoImage(file='weather_icons/{}.png'.format(weather[3]))
+        image['image'] = icon_ref
+        image.image = icon_ref
+        temp_lbl['text'] = '{}°C'.format(weather[2])
+        weather_lbl['text'] = weather[4]
+        humid_lbl['text'] = '{}%'.format(weather[5])
 
-textField = tk.Entry(canvas, font = t)
-textField.pack(pady = 20)
-textField.focus()
-textField.bind('<Return>', func)
-
-L1 = tk.Label(canvas, font = t)
-L1.pack()
-L2 = tk.Label(canvas, font = f)
-L2.pack()
-
-canvas.mainloop()
-
-
+    else:
+        # error message for invalid input
+        messagebox.showerror('Error', 'Cannot find city {}'.format(city))
 
 
+# setting the app window
+app = Tk()
+app.geometry("700x500")
+app.title("Weather")
+city_text = StringVar()
+city_entry = Entry(app, textvariable=city_text)
+city_entry.pack()
 
+# setting the search button
+search_btn = Button(app, text='Search', width=15, command=search)
+search_btn.pack()
 
+# setting the location label
+location_lbl = Label(app, text='Location', font=('Bold', 20))
+location_lbl.pack()
 
+# setting the weather image
+image = Label(app, image='')
+image.pack()
 
+# setting the temperature label
+temp_lbl = Label(app, text='Temperature')
+temp_lbl.pack()
+
+# setting the weather label
+weather_lbl = Label(app, text='Weather')
+weather_lbl.pack()
+
+# setting the humidity label
+humid_lbl = Label(app, text='Humidity')
+humid_lbl.pack()
+
+app.mainloop()
